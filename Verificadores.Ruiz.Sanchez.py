a1c1ea5a208ede2f774b9e6b3502a5ce6ace0de8
Naster20 <57922413+naster20@users.noreply.github.com>
2019-11-30 14:49:17 -0500

    Verificadores de tipo de dato

---
+++ Verificadores.Ruiz.Sanchez.py
@@ -0,0 +1,499 @@
+#EJERCICIO 1
+producto="Leche"
+type_producto=type(producto)
+producto_es_str=isinstance(producto,str)
+producto_es_int=isinstance(producto,int)
+print("Variable Producto es :",type_producto)
+print("Variable es str :",producto_es_str)
+print("Variable es int :",producto_es_int)
+#Fin if
+
+#EJERCICIO 2
+cliente="Naster"
+type_cliente=type(cliente)
+cliente_es_int=isinstance(cliente,int)
+cliente_es_str=isinstance(cliente,str)
+print("Variable Cliente es :",type_producto)
+print("Variable es int :",cliente_es_int)
+print("Variable es str :",cliente_es_str)
+#Fin if
+
+#EJERCICIO 3
+precio=85.3
+type_precio=type(precio)
+precio_es_str=isinstance(precio,str)
+precio_es_float=isinstance(precio,float)
+print("Variable Precio es :",type_precio)
+print("Variable es str :",precio_es_str)
+print("Variable es float :",precio_es_float)
+#Fin if
+
+#EJERCICIO 4
+cant_prod_1=15
+type_cant_prod_1=type(cant_prod_1)
+cant_prod_1_es_bool=isinstance(cant_prod_1,bool)
+cant_prod_1_es_int=isinstance(cant_prod_1,int)
+print("Variable Cantidad de Producto 1 es :",type_cant_prod_1)
+print("Variable es Bool :",cant_prod_1_es_bool)
+print("Variable es Int :",cant_prod_1_es_int)
+#Fin if
+
+#EJERCICIO 5
+costo_uni_prod1=0.0
+type_costo_uni_prod1=type(costo_uni_prod1)
+costo_uni_prod1_es_str=isinstance(costo_uni_prod1,str)
+costo_uni_prod1_es_float=isinstance(costo_uni_prod1,float)
+print("Variable Costo Unitario de Producto 1 es :",type_costo_uni_prod1)
+print("Variable es Str :",costo_uni_prod1_es_str)
+print("Variable es Float :",costo_uni_prod1_es_float)
+#Fin if
+
+#EJERCICIO 6
+alumno="Ruiz Sánchez"
+type_alumno=type(alumno)
+alumno_es_str=isinstance(alumno,str)
+alumno_es_float=isinstance(alumno,float)
+print("Variable Alumno es :",type_alumno)
+print("Variable es Str :",alumno_es_str)
+print("Variable es Float :",alumno_es_float)
+#Fin if
+
+#EJERCICIO 7
+nota1=0.0
+type_nota1=type(nota1)
+nota1_es_str=isinstance(nota1,str)
+nota1_es_float=isinstance(nota1,float)
+print("Variable Nota 1 es :",type_nota1)
+print("Variable es Str :",nota1_es_str)
+print("Variable es Float :",nota1_es_float)
+#Fin if
+
+#EJERCICIO 8
+prom=14.5
+type_prom=type(prom)
+prom_es_float=isinstance(prom,float)
+prom_es_int=isinstance(prom,int)
+print("Variable Promedio es :",type_prom)
+print("Variable es Float :",prom_es_float)
+print("Variable es Int :",prom_es_int)
+#Fin if
+
+#EJERCICIO 9
+persona="Claudio Cordova"
+type_persona=type(persona)
+persona_es_float=isinstance(persona,float)
+persona_es_str=isinstance(persona,str)
+print("Variable Persona es :",type_persona)
+print("Variable es Float :",persona_es_float)
+print("Variable es Str :",persona_es_str)
+#Fin if
+
+#EJERCICIO 10
+otros_gastos_2005=316
+type_otros_gastos_2005=type(otros_gastos_2005)
+otros_gastos_2005_es_float=isinstance(otros_gastos_2005,float)
+otros_gastos_2005_es_int=isinstance(otros_gastos_2005,int)
+print("Variable Otros gastos del 2005 es :",type_otros_gastos_2005)
+print("Variable es Float :",otros_gastos_2005_es_float)
+print("Variable es Int :",otros_gastos_2005_es_int)
+#Fin if
+
+#EJERCICIO 11
+total_de_otros_gastos=26528
+type_total_de_otros_gastos=type(total_de_otros_gastos)
+total_de_otros_gastos_es_int=isinstance(total_de_otros_gastos,int)
+total_de_otros_gastos_es_bool=isinstance(total_de_otros_gastos,bool)
+print("Variable Total de otros gastos es :",type_total_de_otros_gastos)
+print("Variable es Int :",total_de_otros_gastos_es_int)
+print("Variable es Bool :",total_de_otros_gastos_es_bool)
+#Fin if
+
+#EJERCICIO 12
+utilidad_bruta_2005=6050
+type_utilidad_bruta_2005=type(utilidad_bruta_2005)
+utilidad_bruta_2005_es_int=isinstance(utilidad_bruta_2005,int)
+utilidad_bruta_2005_es_float=isinstance(utilidad_bruta_2005,float)
+print("Variable Utilidad bruta del 2005 es :",type_total_de_otros_gastos)
+print("Variable es Int :",utilidad_bruta_2005_es_int)
+print("Variable es Float :",utilidad_bruta_2005_es_float)
+#Fin if
+
+#EJERCICIO 13
+total_de_utilidad_bruta=87543
+type_total_de_utilidad_bruta=type(total_de_utilidad_bruta)
+total_de_utilidad_bruta_es_float=isinstance(total_de_utilidad_bruta,float)
+total_de_utilidad_bruta_es_int=isinstance(total_de_utilidad_bruta,int)
+print("Variable Total de utilidad bruta es :",type_total_de_utilidad_bruta)
+print("Variable es Float :",total_de_utilidad_bruta_es_float)
+print("Variable es Int :",total_de_utilidad_bruta_es_int)
+#Fin if
+
+#EJERCICIO 14
+flujo_de_operacion_2005=3557
+type_flujo_de_operacion_2005=type(flujo_de_operacion_2005)
+flujo_de_operacion_2005_es_float=isinstance(flujo_de_operacion_2005,float)
+flujo_de_operacion_2005_es_int=isinstance(flujo_de_operacion_2005,int)
+print("Variable Flujo de operacion 2005 es :",type_flujo_de_operacion_2005)
+print("Variable es Float :",flujo_de_operacion_2005_es_float)
+print("Variable es Int :",flujo_de_operacion_2005_es_int)
+#Fin if
+
+#EJERCICIO 15
+total_flujo_de_operacion=29504
+type_total_flujo_de_operacion=type(total_flujo_de_operacion)
+total_flujo_de_operacion_es_int=isinstance(total_flujo_de_operacion,int)
+total_flujo_de_operacion_es_str=isinstance(total_flujo_de_operacion,str)
+print("Variable Total de flujo de operacion es :",type_total_flujo_de_operacion)
+print("Variable es Int :",total_flujo_de_operacion_es_int)
+print("Variable es Str :",total_flujo_de_operacion_es_str)
+#Fin if
+
+#EJERCICIO 16
+capital_contable_2005=10357
+type_capital_contable_2005=type(capital_contable_2005)
+capital_contable_2005_es_int=isinstance(capital_contable_2005,int)
+capital_contable_2005_es_str=isinstance(capital_contable_2005,str)
+print("Variable Capital contables del 2005 es :",type_capital_contable_2005)
+print("Variable es Int :",capital_contable_2005_es_int)
+print("Variable es Str :",capital_contable_2005_es_str)
+#Fin if
+
+#EJERCICIO 17
+total_capital_contable="15089343"
+type_total_capital_contable=type(total_capital_contable)
+total_capital_contable_es_int=isinstance(total_capital_contable,int)
+total_capital_contable_es_str=isinstance(total_capital_contable,str)
+print("Variable Total de capital contable es :",type_total_capital_contable)
+print("Variable es Int :",total_capital_contable_es_int)
+print("Variable es Str :",total_capital_contable_es_str)
+#Fin if
+
+#EJERCICIO 18
+participacion_controladora_2005=9825
+type_participacion_controladora_2005=type(participacion_controladora_2005)
+participacion_controladora_2005_es_int=isinstance(participacion_controladora_2005,int)
+participacion_controladora_2005_es_str=isinstance(participacion_controladora_2005,str)
+print("Variable Participacion controladora es :",type_participacion_controladora_2005)
+print("Variable es Int :",participacion_controladora_2005_es_int)
+print("Variable es Str :",participacion_controladora_2005_es_str)
+#Fin if
+
+#EJERCICIO 19
+total_participacion_controladora=35829
+type_total_participacion_controladora=type(total_participacion_controladora)
+total_participacion_controladora_es_int=isinstance(total_participacion_controladora,int)
+total_participacion_controladora_es_str=isinstance(total_participacion_controladora,str)
+print("Variable Total de participacion controladora es :",type_total_participacion_controladora)
+print("Variable es Int :",total_participacion_controladora_es_int)
+print("Variable es Str :",total_participacion_controladora_es_str)
+#Fin if
+
+#EJERCICIO 20
+hipotenusa=14.7
+type_hipotenusa=type(hipotenusa)
+hipotenusa_es_float=isinstance(hipotenusa,float)
+hipotenusa_es_int=isinstance(hipotenusa,int)
+print("Variable Hipotenusa es :",type_hipotenusa)
+print("Variable es Float :",hipotenusa_es_float)
+print("Variable es Int :",hipotenusa_es_int)
+#Fin if
+
+#EJERCICIO 21
+pi=3.14159
+type_pi=type(pi)
+pi_es_float=isinstance(pi,float)
+pi_es_int=isinstance(pi,int)
+print("Variable PI es :",type_pi)
+print("Variable es Float :",pi_es_float)
+print("Variable es Int :",pi_es_int)
+#Fin if
+
+#EJERCICIO 22
+radio=4
+type_radio=type(radio)
+radio_es_float=isinstance(radio,float)
+radio_es_int=isinstance(radio,int)
+print("Variable PI es :",type_radio)
+print("Variable es Float :",radio_es_float)
+print("Variable es Int :",radio_es_int)
+#Fin if
+
+#EJERCICIO 23
+clave="Find20ruiZ"
+type_clave=type(clave)
+clave_es_float=isinstance(clave,float)
+clave_es_str=isinstance(clave,str)
+print("Variable Clave es :",type_clave)
+print("Variable es Float :",clave_es_float)
+print("Variable es Str :",clave_es_str)
+#Fin if
+
+#EJERCICIO 24
+apellido="Ruiz Sánchez"
+type_apellido=type(apellido)
+apellido_es_float=isinstance(apellido,float)
+apellido_es_str=isinstance(apellido,str)
+print("Variable Apellido es :",type_apellido)
+print("Variable es Float :",apellido_es_float)
+print("Variable es Str :",apellido_es_str)
+#Fin if
+
+#EJERCICIO 25
+pago_total=930
+type_pago_total=type(pago_total)
+pago_total_es_int=isinstance(pago_total,int)
+pago_total_es_str=isinstance(pago_total,str)
+print("Variable Pago total es :",type_pago_total)
+print("Variable es Int :",pago_total_es_int)
+print("Variable es Str :",pago_total_es_str)
+#Fin if
+
+#EJERCICIO 26
+horas_extras=6
+type_horas_extras=type(horas_extras)
+horas_extras_es_int=isinstance(horas_extras,int)
+horas_extras_es_str=isinstance(horas_extras,str)
+print("Variable Horas extra es :",type_horas_extras)
+print("Variable es Int :",horas_extras_es_int)
+print("Variable es Str :",horas_extras_es_str)
+#Fin if
+
+#EJERCICIO 27
+facultad="FACFyM"
+type_facultad=type(facultad)
+facultad_es_int=isinstance(facultad,int)
+facultad_es_str=isinstance(facultad,str)
+print("Variable Facultad es :",type_facultad)
+print("Variable es Int :",facultad_es_int)
+print("Variable es Str :",facultad_es_str)
+#Fin if
+
+#EJERCICIO 28
+lugar_nacimiento="Cajamarca-Chota"
+type_lugar_nacimiento=type(lugar_nacimiento)
+lugar_nacimiento_es_bool=isinstance(lugar_nacimiento,bool)
+lugar_nacimiento_es_str=isinstance(lugar_nacimiento,str)
+print("Variable Facultad es :",type_lugar_nacimiento)
+print("Variable es Bool :",lugar_nacimiento_es_bool)
+print("Variable es Str :",lugar_nacimiento_es_str)
+#Fin if
+
+#EJERCICIO 29
+sexo=True
+type_sexo=type(sexo)
+sexo_es_bool=isinstance(sexo,bool)
+sexo_es_str=isinstance(sexo,str)
+print("Variable Sexo es :",type_sexo)
+print("Variable es Bool :",sexo_es_bool)
+print("Variable es Str :",sexo_es_str)
+#Fin if
+
+#EJERCICIO 30
+fecha= "30/11/2019"
+type_fecha=type(fecha)
+fecha_es_bool=isinstance(fecha,bool)
+fecha_es_str=isinstance(fecha,str)
+print("Variable Fecha es :",type_fecha)
+print("Variable es Bool :",fecha_es_bool)
+print("Variable es Str :",fecha_es_str)
+#Fin if
+
+#EJERCICIO 31
+ruc="10701919314"
+type_ruc=type(ruc)
+ruc_es_str=isinstance(ruc,str)
+ruc_es_float=isinstance(ruc,float)
+print("Variable RUC es :",type_ruc)
+print("Variable es Str :",ruc_es_str)
+print("Variable es Float :",ruc_es_float)
+#Fin if
+
+#EJERCICIO 32
+boleta_de_venta="000139"
+type_boleta_de_venta=type(boleta_de_venta)
+boleta_de_venta_es_str=isinstance(boleta_de_venta,str)
+boleta_de_venta_es_float=isinstance(boleta_de_venta,float)
+print("Variable N° de boleta de venta es :",type_boleta_de_venta)
+print("Variable es Str :",boleta_de_venta_es_str)
+print("Variable es Float :",boleta_de_venta_es_float)
+#Fin if
+
+#EJERCICIO 33
+declarante="Miguel Angel Gonzalez Yupanqui"
+type_declarante=type(declarante)
+declarante_es_int=isinstance(declarante,int)
+declarante_es_str=isinstance(declarante,str)
+print("Variable Declarante es :",type_declarante)
+print("Variable es Int :",declarante_es_int)
+print("Variable es Str :",declarante_es_str)
+#Fin if
+
+#EJERCICIO 34
+talla=1.65
+type_talla=type(talla)
+talla_es_float=isinstance(talla,float)
+talla_es_str=isinstance(talla,str)
+print("Variable Talla es :",type_talla)
+print("Variable es Float :",talla_es_float)
+print("Variable es Str :",talla_es_str)
+#Fin if
+
+#EJERCICIO 35
+peso=83.5
+type_peso=type(peso)
+peso_es_float=isinstance(peso,float)
+peso_es_str=isinstance(peso,str)
+print("Variable Peso es :",type_peso)
+print("Variable es Float :",peso_es_float)
+print("Variable es Str :",peso_es_str)
+#Fin if
+
+#EJERCICIO 36
+descuento=30
+type_descuento=type(descuento)
+descuento_es_float=isinstance(descuento,float)
+descuento_es_int=isinstance(descuento,int)
+print("Variable Descuento es :",type_descuento)
+print("Variable es Float :",descuento_es_float)
+print("Variable es Int :",descuento_es_int)
+#Fin if
+
+#EJERCICIO 37
+edad=23
+type_edad=type(edad)
+edad_es_int=isinstance(edad,int)
+edad_es_bool=isinstance(edad,bool)
+print("Variable Edad es :",type_edad)
+print("Variable es Int :",edad_es_int)
+print("Variable es Bool :",edad_es_bool)
+#Fin if
+
+#EJERCICIO 38
+tercio_superior=True
+type_tercio_superior=type(tercio_superior)
+tercio_superior_es_float=isinstance(tercio_superior,float)
+tercio_superior_es_bool=isinstance(tercio_superior,bool)
+print("Variable Tercio superior es :",type_tercio_superior)
+print("Variable es Float :",tercio_superior_es_float)
+print("Variable es Bool :",tercio_superior_es_bool)
+#Fin if
+
+#EJERCICIO 39
+bonificacion=320.40
+type_bonificacion=type(bonificacion)
+bonificacion_es_float=isinstance(bonificacion,float)
+bonificacion_es_bool=isinstance(bonificacion,bool)
+print("Variable Bonificacion es :",type_bonificacion)
+print("Variable es Float :",bonificacion_es_float)
+print("Variable es Bool :",bonificacion_es_bool)
+#Fin if
+
+#EJERCICIO 40
+mes="Noviembre"
+type_mes=type(mes)
+mes_es_float=isinstance(mes,float)
+mes_es_str=isinstance(mes,str)
+print("Variable Mes es :",type_mes)
+print("Variable es Float :",mes_es_float)
+print("Variable es str :",mes_es_str)
+#Fin if
+
+#EJERCICIO 41
+codigo="020120275-H"
+type_codigo=type(codigo)
+codigo_es_float=isinstance(codigo,float)
+codigo_es_str=isinstance(codigo,str)
+print("Variable Codigo es :",type_codigo)
+print("Variable es Float :",codigo_es_float)
+print("Variable es str :",codigo_es_str)
+#Fin if
+
+#EJERCICIO 42
+cateto1=3.5
+type_cateto1=type(cateto1)
+cateto1_es_float=isinstance(cateto1,float)
+cateto1_es_str=isinstance(cateto1,str)
+print("Variable Cateto 1 es :",type_cateto1)
+print("Variable es Float :",cateto1_es_float)
+print("Variable es str :",cateto1_es_str)
+#Fin if
+
+#EJERCICIO 43
+cateto2=2.5
+type_cateto2=type(cateto2)
+cateto2_es_float=isinstance(cateto2,float)
+cateto2_es_str=isinstance(cateto2,str)
+print("Variable Cateto 2 es :",type_cateto1)
+print("Variable es Float :",cateto2_es_float)
+print("Variable es str :",cateto2_es_str)
+#Fin if
+
+#EJERCICIO 44
+area_circulo="25.2"
+type_area_circulo=type(area_circulo)
+area_circulo_es_float=isinstance(area_circulo,float)
+area_circulo_es_str=isinstance(area_circulo,str)
+print("Variable Area circulo es :",type_area_circulo)
+print("Variable es Float :",area_circulo_es_float)
+print("Variable es str :",area_circulo_es_str)
+#Fin if
+
+#EJERCICIO 45
+ano=2019
+type_ano=type(ano)
+ano_es_int=isinstance(ano,int)
+ano_es_str=isinstance(ano,str)
+print("Variable Año es :",type_ano)
+print("Variable es Int :",ano_es_int)
+print("Variable es Str :",ano_es_str)
+#Fin if
+
+#EJERCICIO 46
+precio_horas_extras=5.80
+type_precio_horas_extras=type(precio_horas_extras)
+precio_horas_extras_es_int=isinstance(precio_horas_extras,int)
+precio_horas_extras_es_float=isinstance(precio_horas_extras,float)
+print("Variable Precio de horas extra es :",type_precio_horas_extras)
+print("Variable es Int :",precio_horas_extras_es_int)
+print("Variable es Float :",precio_horas_extras_es_float)
+#Fin if
+
+#EJERCICIO 47
+indice_de_masa_corporal=30.023
+type_indice_de_masa_corporal=type(indice_de_masa_corporal)
+indice_de_masa_corporal_es_int=isinstance(indice_de_masa_corporal,int)
+indice_de_masa_corporal_es_float=isinstance(indice_de_masa_corporal,float)
+print("Variable Indice de masa corporal :",type_indice_de_masa_corporal)
+print("Variable es Int :",indice_de_masa_corporal_es_int)
+print("Variable es Float :",indice_de_masa_corporal_es_float)
+#Fin if
+
+#EJERCICIO 48
+dni="48427829"
+type_dni=type(dni)
+dni_es_int=isinstance(dni,int)
+dni_es_str=isinstance(dni,str)
+print("Variable DNI es :",type_dni)
+print("Variable es Int :",dni_es_int)
+print("Variable es Str :",dni_es_str)
+#Fin if
+
+#EJERCICIO 49
+departamento="Lambayeque"
+type_departamento=type(departamento)
+departamento_es_int=isinstance(departamento,int)
+departamento_es_str=isinstance(departamento,str)
+print("Variable Departamento es :",type_departamento)
+print("Variable es Int :",departamento_es_int)
+print("Variable es Str :",departamento_es_str)
+#Fin if
+
+#EJERCICIO 50
+imprenta="KARIBET"
+type_imprenta=type(imprenta)
+imprenta_es_int=isinstance(imprenta,int)
+imprenta_es_str=isinstance(imprenta,str)
+print("Variable Imprenta es :",type_imprenta)
+print("Variable es Int :",imprenta_es_int)
+print("Variable es Str :",imprenta_es_str)
+#Fin if
