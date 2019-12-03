c92571e437180d026b7c4866e466d436b92c2205
Naster20 <57922413+naster20@users.noreply.github.com>
2019-12-02 23:59:52 -0500

    Verificadores de tipo de dato Palacios Gonzales

---
+++ Palacios_Gonzales_lewis.py
@@ -0,0 +1,494 @@
+#Ejercicio 1
+producto="Libro de Auditoria Tributaria"
+type_producto=type(producto)
+producto_es_float=isinstance(producto,float)
+producto_es_str=isinstance(producto,str)
+print("La variable producto es:",type_producto)
+print("La variable es float :",producto_es_float)
+print("La variable es str :",producto_es_str)
+#finif
+
+#Ejercicio 2
+precio=250
+type_precio=type(precio)
+precio_es_int=isinstance(precio,int)
+precio_es_float=isinstance(precio,float)
+print("La variable precio es :",type_precio)
+print("La variable es int :",precio_es_int)
+print("La variable es float :",precio_es_float)
+#finif
+
+#Ejercio 3
+alumno="Lewis"
+type_alumno=type(alumno)
+alumno_es_float=isinstance(alumno,float)
+alumno_es_str=isinstance(alumno,str)
+print("La variable alumno es :",type_alumno)
+print("La variable es float :",alumno_es_float)
+print("La variable es str :",alumno_es_str)
+#finif
+
+#Ejercicio 4
+visitas=36
+type_visitas=type(visitas)
+visitas_es_bool=isinstance(visitas,bool)
+visitas_es_int=isinstance(visitas,int)
+print("La variable visitas es :",type_visitas)
+print("La variable es bool :",visitas_es_bool)
+print("La variable es int :",visitas_es_int)
+#finif
+
+#Ejercicio 5
+operating_system="IOS"
+type_operating_system=type(operating_system)
+operating_system_es_int=isinstance(operating_system,int)
+operating_system_es_str=isinstance(operating_system,str)
+print("La variable operating system es :",type_operating_system)
+print("La variable es int :",operating_system_es_int)
+print("La variable es str :",operating_system_es_str)
+#finif
+
+#Ejercicio 6
+total=214
+type_total=type(total)
+total_es_int=isinstance(total,int)
+total_es_float=isinstance(total,float)
+print("La variable total es :",type_total)
+print("La variable es int :",total_es_int)
+print("La variable es flota :",total_es_float)
+#finif
+
+#Ejercicio 7
+hombre=True
+type_hombre=type(hombre)
+hombre_es_str=isinstance(hombre,str)
+hombre_es_bool=isinstance(hombre,bool)
+print("La variable hombre es :",type_hombre)
+print("La variable es str :",hombre_es_str)
+print("La variable es bool :",hombre_es_bool)
+#finif
+
+#Ejercicio 8
+social_network="Google"
+type_social_network=type(social_network)
+social_network_es_str=isinstance(social_network,str)
+social_network_es_float=isinstance(social_network,float)
+print("La variable social network es :",type_social_network)
+print("La variable es str :",social_network_es_str)
+print("La variable es float :",social_network_es_float)
+#finif
+
+#Ejercicio 9
+cantidad=1
+type_cantidad=type(cantidad)
+cantidad_es_bool=isinstance(cantidad,bool)
+cantidad_es_int=isinstance(cantidad,int)
+print("La variable cantidad es :",type_cantidad)
+print("La variable es bool :",cantidad_es_bool)
+print("La variable es int :",cantidad_es_int)
+#finif
+
+#Ejercicio 10
+promedio=23.5
+type_promedio=type(promedio)
+promedio_es_float=isinstance(promedio,float)
+promedio_es_str=isinstance(promedio,str)
+print("La variable promedio es :",type_promedio)
+print("La variables es float :",promedio_es_float)
+print("La variable es str:",promedio_es_str)
+#finif
+
+#Ejercicio 11
+ganancia=False
+type_ganancia=type(ganancia)
+ganancia_es_bool=isinstance(ganancia,bool)
+ganancia_es_float=isinstance(ganancia,float)
+print("La variable ganancia es :",type_ganancia)
+print("La variable es bool :",ganancia_es_bool)
+print("La variable es float :",ganancia_es_float)
+#finif
+
+#Ejercicio 12
+google=25
+type_google=type(google)
+google_es_str=isinstance(google,str)
+google_es_int=isinstance(google,int)
+print("La variable google es :",type_google)
+print("La variable es str :",google_es_str)
+print("La variable es int :",google_es_int)
+#finif
+
+#Ejercicio 14
+ios=36
+type_ios=type(ios)
+ios_es_bool=isinstance(ios,bool)
+ios_es_int=isinstance(ios,int)
+print("La variable ios es :",type_ios)
+print("La variable es bool :",ios_es_bool)
+print("La variable es int :",ios_es_int)
+#finif
+
+#Ejercicio 15
+ventas_netas=32145
+type_ventas_netas=type(ventas_netas)
+ventas_netas_es_float=isinstance(ventas_netas,float)
+ventas_netas_es_int=isinstance(ventas_netas,int)
+print("La variable ventas netas es :",type_ventas_netas)
+print("La variable es float :",ventas_netas_es_float)
+print("La variable es int :",ventas_netas_es_int)
+#finif
+
+#Ejercicio 16
+kilogramos=16.52
+type_kilogramos=type(kilogramos)
+kilogramos_es_float=isinstance(kilogramos,float)
+kilogramos_es_int=isinstance(kilogramos,int)
+print("La variable kilogramos es :",type_kilogramos)
+print("La variable es float :",kilogramos_es_float)
+print("La variable es int :",kilogramos_es_int)
+#finif
+
+#Ejercicio 17
+gerente="Hugo"
+type_gerente=type(gerente)
+gerente_es_int=isinstance(gerente,int)
+gerente_es_str=isinstance(gerente,str)
+print("La variable gerente es :",type_gerente)
+print("La variable es int :",gerente_es_int)
+print("La variable es str :",gerente_es_str)
+#finif
+
+#Ejercicio 18
+r_u_c="20486760576"
+type_r_u_c=type(r_u_c)
+r_u_c_es_str=isinstance(r_u_c,str)
+r_u_c_es_bool=isinstance(r_u_c,bool)
+print("La variable R.U.C es :",type_r_u_c)
+print("La variable es str :",r_u_c_es_str)
+print("La variable es bool :",r_u_c_es_bool)
+#finif
+
+#Ejercicio 19
+imprenta="KARIBET"
+type_imprenta=type(imprenta)
+imprenta_es_float=isinstance(imprenta,float)
+imprenta_es_str=isinstance(imprenta,str)
+print("La variable imprenta es :",type_imprenta)
+print("La variable es float :",imprenta_es_float)
+print("La variable es str :",imprenta_es_str)
+#finif
+
+#Ejercicio 20
+fecha="26/07/2013"
+type_fecha=type(fecha)
+fecha_es_bool=isinstance(fecha,bool)
+fecha_es_str=isinstance(fecha,str)
+print("La variable fecha es :",type_fecha)
+print("La variable es bool :",fecha_es_bool)
+print("La variable es str :",fecha_es_str)
+#finif
+
+#Ejercicio 21
+precio_mouse=300.00
+type_precio_mouse=type(precio_mouse)
+precio_mouse_es_str=isinstance(precio_mouse,str)
+precio_mouse_es_float=isinstance(precio_mouse,float)
+print("La variable precio mouse es :",type_precio_mouse)
+print("La variable es str :",precio_mouse_es_str)
+print("La variable es float :",precio_mouse_es_float)
+#finif
+
+#Ejercicio 22
+comprar=True
+type_comprar=type(comprar)
+comprar_es_float=isinstance(comprar,float)
+comprar_es_bool=isinstance(comprar,bool)
+print("La variable comprar es :",type_comprar)
+print("La variable es float :",comprar_es_float)
+print("La variable es bool :",comprar_es_bool)
+#finif
+
+#Ejercicio 23
+precio_teclado=123.5
+type_precio_teclado=type(precio_teclado)
+precio_teclado_es_str=isinstance(precio_teclado,str)
+precio_teclado_es_float=isinstance(precio_teclado,float)
+print("La variable precio teclado es :",type_precio_teclado)
+print("La variable es str :",precio_teclado_es_str)
+print("La variable es float :",precio_teclado_es_float)
+#finif
+
+#Ejercicio 24
+cliente="Jorge"
+type_cliente=type(cliente)
+cliente_es_str=isinstance(cliente,str)
+cliente_es_int=isinstance(cliente,int)
+print("La variable cliente es :",type_cliente)
+print("La variable es str :",cliente_es_str)
+print("La variable es int :",cliente_es_int)
+#finif
+
+#Ejercicio 25
+vender=False
+type_vender=type(vender)
+vender_es_str=isinstance(vender,str)
+vender_es_bool=isinstance(vender,bool)
+print("La variable vender es :",type_vender)
+print("La variable es str :",vender_es_str)
+print("La variable es bool :",vender_es_bool)
+#finif
+
+#Ejercicio 26
+venta=65
+type_venta=type(venta)
+venta_es_bool=isinstance(venta,bool)
+venta_es_int=isinstance(venta,int)
+print("La variable venta es :",type_venta)
+print("La variable es bool :",venta_es_bool)
+print("La variable es int :",venta_es_int)
+#finif
+
+#Ejercicio 27
+dolares=314
+type_dolares=type(dolares)
+dolares_es_str=isinstance(dolares,str)
+dolares_es_int=isinstance(dolares,int)
+print("La variable dolar es :",type_dolares)
+print("La variable es str :",dolares_es_str)
+print("La variable es int :",dolares_es_int)
+#finif
+
+#Ejercicio 28
+euros=54
+type_euros=type(euros)
+euros_es_bool=isinstance(euros,bool)
+euros_es_int=isinstance(euros,int)
+print("La variable euro es :",type_euros)
+print("La variable es bool :",euros_es_bool)
+print("La variable es int :",euros_es_int)
+#finif
+
+#Ejercicio 29
+compro=True
+type_compro=type(compro)
+compro_es_bool=isinstance(compro,bool)
+compro_es_str=isinstance(compro,str)
+print("La variable compro es :",type_compro)
+print("La variable bool es:",compro_es_bool)
+print("La variable str es :",compro_es_str)
+#finif
+
+#Ejercicio 30
+i_g_v=15.2
+type_i_g_v=type(i_g_v)
+i_g_v_es_float=isinstance(i_g_v,float)
+i_g_v_es_int=isinstance(i_g_v,int)
+print("La variable I.G.V es :",type_i_g_v)
+print("La variable es float :",i_g_v_es_float)
+print("la variable es int :",i_g_v_es_int)
+#finif
+
+#Ejercicio 31
+impuesto=32.9
+type_impuesto=type(impuesto)
+impuesto_es_int=isinstance(impuesto,int)
+impuesto_es_float=isinstance(impuesto,float)
+print("La variable impuesto es :",type_impuesto)
+print("La variable es int :",impuesto_es_int)
+print("La variable es float :",impuesto_es_float)
+#finif
+
+#Ejercicio 32
+vuelto=0.03
+type_vuelto=type(vuelto)
+vuelto_es_bool=isinstance(vuelto,bool)
+vuelto_es_float=isinstance(vuelto,float)
+print("La variable vuelto es :",type_vuelto)
+print("La variable es bool :",vuelto_es_bool)
+print("La variable es float :",vuelto_es_float)
+#finif
+
+#Ejercicio 33
+subir=False
+type_subir=type(subir)
+subir_es_float=isinstance(subir,float)
+subir_es_bool=isinstance(subir,bool)
+print("La variable subir es :",type_subir)
+print("La variable es float :",subir_es_float)
+print("La variable es bool :",subir_es_bool)
+#finif
+
+#Ejercicio 34
+nombre="Juan Jose"
+type_nombre=type(nombre)
+nombre_es_str=isinstance(nombre,str)
+nombre_es_int=isinstance(nombre,int)
+print("La variable nombre es :",type_nombre)
+print("La variable es str :",nombre_es_str)
+print("La variable es int :",nombre_es_int)
+#finif
+
+#Ejercicio 35
+libro="Probabilidad"
+type_libro=type(libro)
+libro_es_float=isinstance(libro,float)
+libro_es_str=isinstance(libro,str)
+print("La variable libro es :",type_libro)
+print("La variable es float :",libro_es_float)
+print("La variable es str :",libro_es_str)
+#finif
+
+#Ejercicio 36
+lapiceros=5
+type_lapiceros=type(lapiceros)
+lapiceros_es_int=isinstance(lapiceros,int)
+lapiceros_es_str=isinstance(lapiceros,str)
+print("La variable lapicero es :",type_lapiceros)
+print("La variable es int :",lapiceros_es_int)
+print("La variable es str :",lapiceros_es_str)
+#finif
+
+#Ejercicio 37
+problemas=9
+type_problemas=type(problemas)
+problemas_es_bool=isinstance(problemas,bool)
+problemas_es_int=isinstance(problemas,int)
+print("La variable problemas es :",type_problemas)
+print("La variable es bool :",problemas_es_bool)
+print("La variable es int :",problemas_es_int)
+#finif
+
+#Ejercicio 38
+celular="654821354"
+type_celular=type(celular)
+celular_es_float=isinstance(celular,float)
+celular_es_str=isinstance(celular,str)
+print("La variable celular es :",type_celular)
+print("La variable es float :",celular_es_float)
+print("La variable es str :",celular_es_str)
+#finif
+
+#Ejercicio 39
+telefono=623598
+type_telefono=type(telefono)
+telefono_es_int=isinstance(telefono,int)
+telefono_es_str=isinstance(telefono,str)
+print("La variable telefono es :",type_telefono)
+print("La variable es int :",telefono_es_int)
+print("La variable es str :",telefono_es_str)
+#finif
+
+#Ejercicio 40
+personas=125
+type_personas=type(personas)
+personas_es_bool=isinstance(personas,bool)
+personas_es_int=isinstance(personas,int)
+print("La variable persona es :",type_personas)
+print("La variable es bool :",personas_es_bool)
+print("La variable es int :",personas_es_int)
+#finif
+
+#Ejercicio 41
+direccion="San diego"
+type_dirreccion=type(direccion)
+direccion_es_bool=isinstance(direccion,bool)
+direccion_es_str=isinstance(direccion,str)
+print("La variable direccion es :",type_dirreccion)
+print("La variable es bool :",direccion_es_bool)
+print("La variable es str :",direccion_es_str)
+#finif
+
+#Ejercicio 42
+departamento="Lambayeque"
+type_departamento=type(departamento)
+departamento_es_str=isinstance(departamento,str)
+departamento_es_float=isinstance(departamento,float)
+print("La variable departamento es :",type_departamento)
+print("La variable es str :",departamento_es_str)
+print("La variable es float :",departamento_es_float)
+#finif
+
+#Ejercicio 43
+edad=52
+type_edad=type(edad)
+edad_es_str=isinstance(edad,str)
+edad_es_int=isinstance(edad,int)
+print("La variable edad es :",type_edad)
+print("La variable es str :",edad_es_str)
+print("La variable es int :",edad_es_int)
+#finif
+
+#Ejercicio 44
+hijos=3
+type_hijos=type(hijos)
+hijos_es_float=isinstance(hijos,float)
+hijos_es_int=isinstance(hijos,int)
+print("La variable hijos es :",type_hijos)
+print("La variable es float :",hijos_es_float)
+print("La variable es int :",hijos_es_int)
+#finif
+
+#Ejercicios 45
+soltero=True
+type_soltero=type(soltero)
+soltero_es_str=isinstance(soltero,str)
+soltero_es_bool=isinstance(soltero,bool)
+print("La variable soltero es :",type_soltero)
+print("La variable es str :",soltero_es_str)
+print("La variable es bool:",soltero_es_bool)
+#finif
+
+#Ejercicio 46
+pais="Peru"
+type_pais=type(pais)
+pais_es_float=isinstance(pais,float)
+pais_es_str=isinstance(pais,str)
+print("La variable pais es :",type_pais)
+print("La variable es float :",pais_es_float)
+print("La variable es str :",pais_es_str)
+#finif
+
+#Ejercicio 47
+divisas=2.35
+type_divisas=type(divisas)
+divisas_es_int=isinstance(divisas,int)
+divisas_es_float=isinstance(divisas,float)
+print("La variable divisas es :",type_divisas)
+print("La variable es int :",divisas_es_int)
+print("La variable es float :",divisas_es_float)
+#finif
+
+#Ejercicio 48
+retencion=15
+type_retencion=type(retencion)
+retencion_es_bool=isinstance(retencion,bool)
+retencion_es_int=isinstance(retencion,int)
+print("La variable retencion es :",type_retencion)
+print("La variable es bool :",retencion_es_bool)
+print("La variable es int :",retencion_es_int)
+#finif
+
+#Ejercicio 49
+i_v_a=25
+type_i_v_a=type(i_v_a)
+i_v_a_es_float=isinstance(i_v_a,float)
+i_v_a_es_int=isinstance(i_v_a,int)
+print("La variable I.V.A es :",type_i_v_a)
+print("La variable es float :",i_v_a_es_float)
+print("La variable es int :",i_v_a_es_int)
+#finif
+
+#Ejercicio 50
+chrome=51.17
+type_chrome=type(chrome)
+chrome_es_str=isinstance(chrome,str)
+chrome_es_float=isinstance(chrome,float)
+print("La variable Chrome es :",type_chrome)
+print("La variable es str :",chrome_es_str)
+print("La variable es float :",chrome_es_float)
+#finif
+
+
+
+
+
